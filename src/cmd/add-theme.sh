af202c2988f57fa3db662198ba787d59c1c011ed
NostraDavid <55331731+NostraDavid@users.noreply.github.com>
2022-11-20 17:55:11 +0100

    add cmd folder

---
+++ src/cmd/add-theme.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# how I added an example theme
+git submodule add https://github.com/theNewDynamic/gohugo-theme-ananke.git themes/ananke
---
+++ src/cmd/start-dev-server.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# run server in development mode; this will show posts with `draft: true` in their header.
+hugo server -D
